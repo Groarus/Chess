16c2cb6009b879d0b824bb4c971ab22b0168444e
Eric <erickollee@hotmail.com>
2014-12-16 23:26:25 -0500

    Shortened Sleep time on HumanPlayer threads

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -37,7 +37,7 @@ public class HumanPlayer extends Player implements Runnable {
     public void run() {
         while (true) {
             try {
-                Thread.sleep(100);
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
